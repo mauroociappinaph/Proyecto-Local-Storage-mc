74238b7f5506976da943dd207cade33401ce196e
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-06-10 00:04:04 -0300

    Add files via upload
    Mostrar los tweets

--- js/app.js
+++ js/app.js
@@ -13,24 +13,57 @@ function agregarTweet(e) {
 
   // Validación
   if (tweet === "") {
-    mostrarError("Un mensaje no puede ir vació");
+    mostrarError("Un mensaje no puede ir vacío");
     return;
   }
-  console.log("agregando tweet");
+  const tweetObj = {
+    id: Date.now(),
+    tweet,
+  };
+
+  // Añadir tweet al arreglo de tweets
+  tweets = [...tweets, tweetObj];
+
+  // Crear HTML
+  crearHTML();
+
+  // Reiniciar formulario
+  formulario.reset();
 }
 
-//Mostrar un mensaje de error
+// Mostrar un mensaje de error
 function mostrarError(error) {
   const mensajeError = document.createElement("p");
   mensajeError.textContent = error;
   mensajeError.classList.add("error");
 
-  //Insertar contenido
+  // Insertar contenido
   const contenido = document.querySelector("#contenido");
   contenido.appendChild(mensajeError);
 
-  //Eliminar alerta después de los 3 seg
+  // Eliminar alerta después de los 3 seg
   setTimeout(() => {
     mensajeError.remove();
   }, 3000);
+}
+
+function crearHTML() {
+  limpiarHTML();
+  if (tweets.length > 0) {
+    tweets.forEach((tweet) => {
+      // Crear HTML
+      const li = document.createElement("li");
+      //Añadir texto
+      li.innerText = tweet.tweet;
+      //Insertar en el HTML
+      listaTweets.appendChild(li);
+    });
+  }
+}
+
+// Limpiar HTML
+function limpiarHTML() {
+  while (listaTweets.firstChild) {
+    listaTweets.removeChild(listaTweets.firstChild);
+  }
 }
